bee482036f3fbf421a115ea6534bf880834f4602
chokwonsik <chokwonsik15@gmail.com>
2020-07-30 15:32:15 +0900

    Initial commit

---
+++ Chokwonsik/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Chokwonsik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServerIPtextBox = new System.Windows.Forms.TextBox();
+            this.ServerPorttextBox = new System.Windows.Forms.TextBox();
+            this.ClientIPtextBox = new System.Windows.Forms.TextBox();
+            this.ClientPorttextBox = new System.Windows.Forms.TextBox();
+            this.ChatScreentextBox = new System.Windows.Forms.TextBox();
+            this.MessagetextBox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ServerIPtextBox
+            // 
+            this.ServerIPtextBox.BackColor = System.Drawing.Color.White;
+            this.ServerIPtextBox.Font = new System.Drawing.Font("a뉴굴림4", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ServerIPtextBox.ForeColor = System.Drawing.Color.Black;
+            this.ServerIPtextBox.Location = new System.Drawing.Point(114, 34);
+            this.ServerIPtextBox.Name = "ServerIPtextBox";
+            this.ServerIPtextBox.Size = new System.Drawing.Size(139, 23);
+            this.ServerIPtextBox.TabIndex = 0;
+            // 
+            // ServerPorttextBox
+            // 
+            this.ServerPorttextBox.BackColor = System.Drawing.Color.White;
+            this.ServerPorttextBox.Font = new System.Drawing.Font("a뉴굴림4", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ServerPorttextBox.ForeColor = System.Drawing.Color.Black;
+            this.ServerPorttextBox.Location = new System.Drawing.Point(383, 34);
+            this.ServerPorttextBox.Name = "ServerPorttextBox";
+            this.ServerPorttextBox.Size = new System.Drawing.Size(130, 23);
+            this.ServerPorttextBox.TabIndex = 1;
+            // 
+            // ClientIPtextBox
+            // 
+            this.ClientIPtextBox.BackColor = System.Drawing.Color.White;
+            this.ClientIPtextBox.Font = new System.Drawing.Font("a뉴굴림4", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ClientIPtextBox.ForeColor = System.Drawing.Color.Black;
+            this.ClientIPtextBox.Location = new System.Drawing.Point(114, 95);
+            this.ClientIPtextBox.Name = "ClientIPtextBox";
+            this.ClientIPtextBox.Size = new System.Drawing.Size(139, 23);
+            this.ClientIPtextBox.TabIndex = 2;
+            // 
+            // ClientPorttextBox
+            // 
+            this.ClientPorttextBox.BackColor = System.Drawing.Color.White;
+            this.ClientPorttextBox.Font = new System.Drawing.Font("a뉴굴림4", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ClientPorttextBox.ForeColor = System.Drawing.Color.Black;
+            this.ClientPorttextBox.Location = new System.Drawing.Point(383, 94);
+            this.ClientPorttextBox.Name = "ClientPorttextBox";
+            this.ClientPorttextBox.Size = new System.Drawing.Size(130, 23);
+            this.ClientPorttextBox.TabIndex = 3;
+            // 
+            // ChatScreentextBox
+            // 
+            this.ChatScreentextBox.BackColor = System.Drawing.Color.White;
+            this.ChatScreentextBox.Font = new System.Drawing.Font("휴먼엑스포", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ChatScreentextBox.ForeColor = System.Drawing.Color.Black;
+            this.ChatScreentextBox.Location = new System.Drawing.Point(33, 170);
+            this.ChatScreentextBox.MaximumSize = new System.Drawing.Size(600, 300);
+            this.ChatScreentextBox.Multiline = true;
+            this.ChatScreentextBox.Name = "ChatScreentextBox";
+            this.ChatScreentextBox.Size = new System.Drawing.Size(600, 163);
+            this.ChatScreentextBox.TabIndex = 4;
+            // 
+            // MessagetextBox
+            // 
+            this.MessagetextBox.BackColor = System.Drawing.Color.White;
+            this.MessagetextBox.Font = new System.Drawing.Font("a뉴굴림4", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.MessagetextBox.ForeColor = System.Drawing.Color.Black;
+            this.MessagetextBox.Location = new System.Drawing.Point(33, 371);
+            this.MessagetextBox.Multiline = true;
+            this.MessagetextBox.Name = "MessagetextBox";
+            this.MessagetextBox.Size = new System.Drawing.Size(500, 50);
+            this.MessagetextBox.TabIndex = 5;
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.White;
+            this.StartButton.Font = new System.Drawing.Font("경기천년제목 Medium", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.StartButton.ForeColor = System.Drawing.Color.Black;
+            this.StartButton.Location = new System.Drawing.Point(527, 34);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(105, 23);
+            this.StartButton.TabIndex = 6;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.BackColor = System.Drawing.Color.White;
+            this.ConnectButton.Font = new System.Drawing.Font("경기천년제목 Medium", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ConnectButton.ForeColor = System.Drawing.Color.Black;
+            this.ConnectButton.Location = new System.Drawing.Point(527, 93);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(105, 23);
+            this.ConnectButton.TabIndex = 7;
+            this.ConnectButton.Text = "CONNECT";
+            this.ConnectButton.UseVisualStyleBackColor = false;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // SendButton
+            // 
+            this.SendButton.BackColor = System.Drawing.Color.White;
+            this.SendButton.Font = new System.Drawing.Font("경기천년제목 Medium", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.SendButton.ForeColor = System.Drawing.Color.Black;
+            this.SendButton.Location = new System.Drawing.Point(557, 370);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 50);
+            this.SendButton.TabIndex = 8;
+            this.SendButton.Text = "SEND";
+            this.SendButton.UseVisualStyleBackColor = false;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker2_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(32, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Server IP";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(287, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Server Port";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(32, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 22);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Client IP";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(287, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 22);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Client Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(676, 457);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.MessagetextBox);
+            this.Controls.Add(this.ChatScreentextBox);
+            this.Controls.Add(this.ClientPorttextBox);
+            this.Controls.Add(this.ClientIPtextBox);
+            this.Controls.Add(this.ServerPorttextBox);
+            this.Controls.Add(this.ServerIPtextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ServerIPtextBox;
+        private System.Windows.Forms.TextBox ServerPorttextBox;
+        private System.Windows.Forms.TextBox ClientIPtextBox;
+        private System.Windows.Forms.TextBox ClientPorttextBox;
+        private System.Windows.Forms.TextBox ChatScreentextBox;
+        private System.Windows.Forms.TextBox MessagetextBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button SendButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Chokwonsik/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace Chokwonsik
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string recieve;
+        public string TextToSend;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach (IPAddress address in localIP)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ServerIPtextBox.Text = address.ToString();
+                }
+            }
+
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(ServerPorttextBox.Text));
+            listener.Start();
+            client = listener.AcceptTcpClient();
+            STR = new StreamReader(client.GetStream());
+            STW = new StreamWriter(client.GetStream());
+            STW.AutoFlush = true;
+
+            backgroundWorker1.RunWorkerAsync();
+            backgroundWorker2.WorkerSupportsCancellation = true;
+
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient();
+            IPEndPoint IpEnd = new IPEndPoint(IPAddress.Parse(ClientIPtextBox.Text), int.Parse(ClientPorttextBox.Text));
+
+            try
+            {
+                client.Connect(IpEnd);
+
+                if (client.Connected)
+                {
+                    ChatScreentextBox.AppendText("Connected to server" + "\r\n");
+                    STW = new StreamWriter(client.GetStream());
+                    STR = new StreamReader(client.GetStream());
+                    STW.AutoFlush = true;
+                    backgroundWorker1.RunWorkerAsync();
+                    backgroundWorker2.WorkerSupportsCancellation = true;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            if (MessagetextBox.Text != "")
+            {
+                TextToSend = MessagetextBox.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            MessagetextBox.Text = "";
+
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    recieve = STR.ReadLine();
+                    this.ChatScreentextBox.Invoke(new MethodInvoker(delegate ()
+                    {
+                        ChatScreentextBox.AppendText("You: " + recieve + "\r\n");
+                    }));
+                    recieve = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void BackgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(TextToSend);
+                this.ChatScreentextBox.Invoke(new MethodInvoker(delegate ()
+                {
+                    ChatScreentextBox.AppendText("Me: " + TextToSend + "\r\n");
+                }));
+            }
+            else
+            {
+                MessageBox.Show("Sending failed");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+    }
+}
